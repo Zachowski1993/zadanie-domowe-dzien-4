74c02227cae6236453614cc404f36c666125f485
Piotr Zachowski <zachowski@outlook.com>
2024-01-10 18:52:13 +0000

    Dodaj pliki projektów.

---
+++ zadanie domowe dzien 4/Program.cs
@@ -0,0 +1,17 @@
+﻿var name = "Kinga";
+var gender = "woman";
+var age = 28;
+
+    if (gender == "woman" && age < 30)
+    {
+    Console.WriteLine ("Kobieta ponizej 30 lat");
+    }
+    else if (name == "Kinga" && age == 30)
+    {
+    Console.WriteLine("Kinga, lat 30");
+    }
+    else if (gender == "man" && age < 18)
+    {
+    Console.WriteLine("Niepelnoletni mezczyzna");
+    }
+    
